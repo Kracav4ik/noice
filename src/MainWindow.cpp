448c4169948ca88ac5517984852a2c4ca2c7f420
Kracav4ik <dikama2013@yandex.ru>
2017-02-07 01:46:36 +0300

    Bifurcation diagram

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -17,6 +17,23 @@ MainWindow::MainWindow() {
         }
     });
 
+    limit->setCallback([this](QPainter& p) {
+        p.setPen(QColor(255, 0, 0, 3));
+        for (int r0 = 0; r0 < 401; r0+=1) {
+            float r = 2.4f + 4*r0/1000.f;
+            for (int n0 = 0; n0 < 201; ++n0) {
+                float n = n0 / 200.f;
+                for (int _ = 0; _ < 300; ++_) {
+                    n = r * n * (1 - n);
+                }
+                for (int _ = 0; _ < 30; ++_) {
+                    n = r * n * (1 - n);
+                    p.drawPoint(r0, 200-(int) (200 * n));
+                }
+            }
+        }
+    });
+
     connect(doubleSpinBox, SIGNAL(valueChanged(double)), widget, SLOT(update()));
 
     show();
