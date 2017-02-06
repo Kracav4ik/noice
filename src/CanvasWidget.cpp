f7c966e29404e24a4402a7e9ab2d81860b94e066
Kracav4ik <dikama2013@yandex.ru>
2017-02-07 00:36:56 +0300

    WTF with alpha

--- src/CanvasWidget.cpp
+++ src/CanvasWidget.cpp
@@ -4,7 +4,7 @@ void CanvasWidget::paintEvent(QPaintEvent *event) {
     QPainter p(this);
     p.fillRect(rect(), QColor(255, 255, 255));
     for (int n0 = 0; n0 < 201; ++n0) {
-        p.setPen(QColor(n0 + 55, 0, 255 - n0));
+        p.setPen(QColor(n0 + 55, 0, 255 - n0, 128));
 //        p.drawLine(0, n0, 300, n0);
         float YY = n0 / 200.f;
         int n = n0;
